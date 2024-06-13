22729952361fcbed43e2018d6d8093282521de01
babimumba <babimumba243@gmail.com>
2024-06-13 15:17:52 +0200

    first commit

---
+++ app/src/main/java/cd/projetthealthcare/com/View/SplashScreenActivity.kt
@@ -0,0 +1,35 @@
+package cd.projetthealthcare.com.View
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import cd.projetthealthcare.com.MainActivity
+import cd.projetthealthcare.com.R
+import cd.projetthealthcare.com.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //fonction pour afficher le splash screen pendant 3 secondes
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
