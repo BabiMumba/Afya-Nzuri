bbf147e1c1450128021573870cf0f77dbe6331df
babimumba <babimumba243@gmail.com>
2024-06-29 23:55:10 +0200

    modification de la page chat ajouter la fonctionnalite de message

---
+++ app/src/main/java/cd/projetthealthcare/com/View/ChatActivity.kt
@@ -0,0 +1,24 @@
+package cd.projetthealthcare.com.View
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import cd.projetthealthcare.com.R
+import cd.projetthealthcare.com.databinding.ActivityChatBinding
+
+class ChatActivity : AppCompatActivity() {
+    lateinit var binding: ActivityChatBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
